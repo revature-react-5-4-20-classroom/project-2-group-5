5d87910c600c02cb0d52b93f7606b27d74c408e4
Steven Gutierrez <stematgut@gmail.com>
2020-06-12 07:41:12 -0700

    Added in cors filter and docket bean as well as swagger2, spring security, and lombok dependencies

--- p2Backend/src/main/java/com/g5/p2/P2BackendApplication.java
+++ p2Backend/src/main/java/com/g5/p2/P2BackendApplication.java
@@ -2,6 +2,12 @@ package com.g5.p2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class P2BackendApplication {
@@ -9,5 +15,38 @@ public class P2BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(P2BackendApplication.class, args);
 	}
+	
+	   /**
+     * CORS Filter Bean to disable CORS
+     * 
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+      // We're defining the class we're using inline here as a shortcut
+      return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+          
+        }
+      };
+    }
+    
+    /**
+     * Swagger "Docket" Bean for all the controllers in our project
+     */
+    @Bean
+    public Docket catApi() {
+      return new Docket(DocumentationType.SWAGGER_2).select()
+          .apis(RequestHandlerSelectors.basePackage("com.g5.p2")).build();
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }

d03c00fb193f2494e590a47ab661eb240751d83e
Steven Gutierrez <stematgut@gmail.com>
2020-06-14 13:35:50 -0700

    created message page folder and file - added box shadow to subsribers page

---
+++ postit/src/components/pages/Messages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Row, Col, Container } from 'reactstrap';
+import './style.css';
+
+export class MessagesPage extends React.Component<any, any> {
+  render() {
+    return (
+      <Container className='main-container'>
+        <Row className='h-100'></Row>
+      </Container>
+    );
+  }
+}

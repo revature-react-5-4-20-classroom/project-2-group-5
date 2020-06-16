9cc62a57261585020358a9c81a47a0351bc41cd5
kruti4 <krutifaldu9@gmail.com>
2020-06-16 00:53:33 -0700

    added endpoint mapping methods for comments

--- p2Backend/src/main/java/com/g5/p2/controllers/CommentsController.java
+++ p2Backend/src/main/java/com/g5/p2/controllers/CommentsController.java
@@ -1,9 +1,20 @@
 package com.g5.p2.controllers;
 
+import java.util.LinkedHashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.g5.p2.models.Comments;
+import com.g5.p2.models.Subscriptions;
 import com.g5.p2.services.CommentsServiceImplementation;
+
+import antlr.collections.List;
 
 @RequestMapping(path = "/comments")
 @RestController
@@ -13,7 +24,23 @@ public class CommentsController {
   CommentsServiceImplementation commentsService;
  
   //get comments by postId
+  @GetMapping("{commentId}")
+  public java.util.List<Comments> getCommentByPostId(@PathVariable Integer commentId){
+	  return commentsService.getAll();
+	
+  }
   
   //create comment
+  @PostMapping
+  public Comments createComment(@RequestBody LinkedHashMap<String, Object> c) {
+      try {
+          Comments convert = new Comments((Integer)c.get("commentId"),(String)c.get("content"));
+          return commentsService.create(convert, (Integer)c.get("postId"),(Integer)c.get("userId"));
+      }
+      catch(Exception e) {
+          e.printStackTrace();
+          throw e;
+      }
+  }
   
 }

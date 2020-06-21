454e40c31a1acfee6a276a0174a673df657eb2ff
kruti4 <krutifaldu9@gmail.com>
2020-06-20 19:14:04 -0700

    modified end pints for pics in front end, service and controllerin backend and added custom exception class for picture not found

--- p2Backend/src/main/java/com/g5/p2/services/PicsService.java
+++ p2Backend/src/main/java/com/g5/p2/services/PicsService.java
@@ -1,7 +1,9 @@
 package com.g5.p2.services;
 
+import java.util.Optional;
 import org.springframework.web.multipart.MultipartFile;
 import com.g5.p2.models.Pics;
+import com.g5.p2.models.Users;
 
 public interface PicsService {
 
@@ -10,4 +12,8 @@ public interface PicsService {
   //Pics savePic(MultipartFile file);
   
   boolean deletePic(Integer picId);
+  
+  Pics saveFile(MultipartFile file, Integer id);
+
+  Optional<Pics> getfile(Integer fileId);
 }
--- p2Backend/src/main/java/com/g5/p2/services/PicsServiceImplementation.java
+++ p2Backend/src/main/java/com/g5/p2/services/PicsServiceImplementation.java
@@ -5,7 +5,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.g5.p2.exceptions.PictureNotFoundException;
 import com.g5.p2.models.Pics;
+import com.g5.p2.models.Users;
 import com.g5.p2.repositories.PicsRepository;
 
 @Service
@@ -15,15 +18,15 @@ public class PicsServiceImplementation implements PicsService {
 
   @Autowired
   PicsRepository picsRepository;
-  
+
   @Override
   public Pics getById(Integer picId) {
-    
+
     return picsRepository.findByPicId(picId);
   }
 
-  //Pics savePic(MultipartFile file);
-  
+  // Pics savePic(MultipartFile file);
+
   @Override
   public boolean deletePic(Integer picId) {
     Optional<Pics> existingPic = picsRepository.findById(picId);
@@ -35,4 +38,39 @@ public class PicsServiceImplementation implements PicsService {
     }
   }
 
+
+
+  @Override
+  public Pics saveFile(MultipartFile file, Integer id) {
+    String docname = file.getOriginalFilename();
+    try {
+//      Users user = this.getById(id);
+//      user.setPictureName(docname);
+//      user.setPictureType(file.getContentType());
+//      user.setPic(file.getBytes());
+
+      // Users user = new Users("username1513", "password15", "alias15", "role15", docname,
+      // file.getContentType(),
+      // file.getBytes());
+      // user.setUserId(usersRepository.findAll().get(usersRepository.findAll().size() -
+      // 1).getUserId() + 1);
+      Pics pic = new Pics(id,docname,file.getContentType(),file.getBytes());
+      return picsRepository.save(pic);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  // getting image by user id
+  @Override
+  public Optional<Pics> getfile(Integer fileId) {
+    Optional<Pics> pic= picsRepository.findById(fileId);
+    if(pic.isPresent()) {
+      return pic;
+    }
+    else {
+     throw new PictureNotFoundException();
+    }
+  }
 }

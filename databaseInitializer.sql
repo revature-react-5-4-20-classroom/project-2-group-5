87defd0f0f18b6465a45012754c1bb8b25674a4f
Steven Gutierrez <stematgut@gmail.com>
2020-06-20 21:01:22 -0700

    Username shows bolded next to reply content.  Posting a reply immediately updates comment div

--- databaseInitializer.sql
+++ databaseInitializer.sql
@@ -59,3 +59,28 @@ INSERT INTO comments(post_id, author, content) values
 
 INSERT INTO messages(author, receiver, content) values
 ((SELECT user_id FROM users WHERE username = 'admin'), (SELECT user_id FROM users WHERE username = 'user'), 'HELLO USER')
+
+
+-- INSERT INTO users values
+-- ('coolguy', 'password', 'frosty', 'user'), -- 1
+-- ('coolgirl', 'password', 'butterfly', 'user'), -- 2
+-- ('coolkid', 'password', 'legokid', 'user'); --3 
+
+-- INSERT INTO posts(author, date_posted, title, content) values 
+-- (2, 20200620, 'Sephora', 'Did yall see the new sale going on at sephora this week?  Definitely going!'), -- 1
+-- (1, 20200620, 'New here', 'How do I use this app?  Just saw it here.'), -- 2
+-- (3, 20200620, 'Minecraft?', 'Just started playing minecraft, how is it?'), -- 3
+-- (3, 20200620, 'Computer overheating', 'So I just downloaded that new filter to make everything look cool on Minecraft and now my computer gets really hot, what should I do?'), -- 4
+-- (1, 20200620, 'Bagels', 'Found a cool bagel shop on 4th street name "Bagels R Us".  Check it out!'), -- 5
+-- (2, 20200620, 'Sold out!', 'Cant believe they already ran out of my favorite brands, some sale!'), -- 6
+-- (3, 20200620, 'Birthday coming up', 'Asking mom and dad for a new laptop for my birthday next month!!!!'), -- 7
+-- (2, 20200620, 'Just kidding!', 'Oh they said I could order online and still get the sales price - happy camper here.'), -- 8
+-- (1, 20200620, 'Hungry', 'Lunch time already and thinking about getting another bagel, should I?'), -- 9
+
+-- INSERT INTO subscriptions(subscribee, subscriber, blocked) values
+-- (2, 1, FALSE),
+-- (3, 1, FALSE),
+-- (1, 2, FALSE),
+-- (3, 2, FALSE),
+-- (2, 3, FALSE),
+-- (1, 3, FALSE);
--- postit/src/models/comment.tsx
+++ postit/src/models/comment.tsx
@@ -1,12 +1,20 @@
-export class Comment{
-    commentId:number;
-    postId:number;
-    author:number;
-    content:String;
-    constructor(commentId:number,postId:number,author:number,content:String){
-        this.commentId=commentId;
-        this.postId=postId;
-        this.author=author;
-        this.content=content;
-    }
+export class Comment {
+  commentId: number;
+  postId: number;
+  author: number;
+  content: String;
+  username?: string;
+  constructor(
+    commentId: number,
+    postId: number,
+    author: number,
+    content: String,
+    username?: string
+  ) {
+    this.commentId = commentId;
+    this.postId = postId;
+    this.author = author;
+    this.content = content;
+    this.username = username;
+  }
 }
